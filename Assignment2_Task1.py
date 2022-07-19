96fba77095cfd9411cdfdf39e4769e931da6fdfb
harry16116 <63609521+harry16116@users.noreply.github.com>
2022-07-19 12:50:35 +0530

    Add files via upload
    Fibonacci series

---
+++ Assignment2_Task1.py
@@ -0,0 +1,12 @@
+x=0
+y=1
+z=int(input("enter how many numbers of fibonacci series you want :"))
+if(z<=0):
+    print('invalid number')
+COUNT = 0
+while COUNT< z :
+    print(x)
+    nth = x+y
+    x = y
+    y = nth
+    COUNT+=1
